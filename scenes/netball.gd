962721601aa4885156fe92ac54c9904e766d7edd
ev4nyan <49965629+ev4nyan@users.noreply.github.com>
2023-01-10 11:07:06 -0500

    jimmy

--- scenes/netball.gd
+++ scenes/netball.gd
@@ -1,24 +1,21 @@
-extends Area2D
+extends KinematicBody2D
 
-onready var destroy_timer = get_node("Destroy_timer")
+var ballspeed = 1
+var velocity : Vector2 = Vector2.ZERO
+
+
+
+
 var velocity = Vector2(1, 0)
-var player_rotation
 
 export(int) var speed = 1400
-export(int) var damage = 25
-
 puppet var puppet_position setget puppet_position_set
 puppet var puppet_velocity = Vector2(0, 0)
-puppet var puppet_rotation = 0
-
 onready var initial_position = global_position
 
 var player_owner = 0
 
 func _ready() -> void:
-	destroy_timer.start()
-	visible = false
-	yield(get_tree(), "idle_frame")
 	
 	if get_tree().has_network_peer():
 		if is_network_master():
@@ -28,7 +25,6 @@ func _ready() -> void:
 			rset("puppet_rotation", rotation)
 			rset("puppet_position", global_position)
 	
-	visible = true
 
 func _process(delta: float) -> void:
 	if get_tree().has_network_peer():
@@ -37,8 +33,6 @@ func _process(delta: float) -> void:
 		else:
 			rotation = puppet_rotation
 			global_position += puppet_velocity * speed * delta
-
-
 
 func puppet_position_set(new_value) -> void:
 	puppet_position = new_value
@@ -51,3 +45,27 @@ func _on_Destroy_timer_timeout():
 	if get_tree().has_network_peer():
 		if get_tree().is_network_server():
 			rpc("destroy")
+
+
+----
+
+
+
+
+
+
+
+
+
+
+func change_speed(direction, speed, multiplier):
+	velocity.x = direction.x * speed
+	velocity.y = direction.y * speed
+	ballspeed *= multiplier
+	print(velocity)
+	
+func _physics_process(delta):
+	var collision_object = move_and_collide(velocity * ballspeed * delta)
+	if collision_object:
+		velocity = velocity.bounce(collision_object.normal)
+
--- scripts/net_player_click.gd
+++ scripts/net_player_click.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
 export(PackedScene) var DAGGER : PackedScene = preload("res://projectiles/wind-aa.tscn")
-#onready var attackTimer = $AATimer
+
+
 onready var tween = $Tween
 
 var speed = 0
@@ -14,7 +15,7 @@ onready var attackTimer = $AATimer
 
 puppet var puppet_position = Vector2.ZERO setget puppet_position_set
 puppet var puppet_velocity = Vector2()
-#puppet var puppet_rotation = 0
+puppet var puppet_rotation = 0
 
 func puppet_position_set(new_value):
 	puppet_position = new_value
@@ -40,10 +41,17 @@ func throw_dagger(dagger_direction):
 func _process(delta):
 	if is_network_master():
 		
+		look_at(get_global_mouse_position())
+
 		if Input.is_action_pressed('click'):
 			moving = true
 			destination = get_global_mouse_position()
 			movement = position.direction_to(destination) * speed
+		
+		
+		if Input.is_action_pressed('aa') and attackTimer.is_stopped():
+			rpc("instance_bullet", get_tree().get_network_unique_id())
+			attackTimer.start()
 		if not moving:
 			speed = 0
 		else:
@@ -56,15 +64,51 @@ func _process(delta):
 		
 		
 		#move_direction = rad2deg(destination.angle_to_point(position))
-		if position.distance_to(destination) > 1:
+		if position.distance_to(destination) > 32:
 			movement = move_and_slide(movement)
 		else:
 			moving = false
 	else:
-#		rotation_degrees = lerp(rotation_degrees, puppet_rotation, delta * 8)
+		rotation = lerp_angle(rotation, puppet_rotation, delta * 8)
 		if not tween.is_active():
 			move_and_slide(puppet_velocity * speed)
 func _on_Network_tick_rate_timeout():
 	if is_network_master():
 		rset_unreliable("puppet_position", global_position)
 		rset_unreliable("puppet_velocity", movement)
+		rset_unreliable("puppet_rotation", rotation)
+
+
+func lerp_angle(from, to, weight):
+	return from + short_angle_dist(from, to) * weight
+
+func short_angle_dist(from, to):
+	var max_angle = PI * 2
+	var difference = fmod(to - from, max_angle)
+	return fmod(2 * difference, max_angle) - difference
+
+
+sync func update_position(pos):
+	global_position = pos
+	puppet_position = pos
+
+func _on_Hitbox_area_entered(area):
+	if get_tree().is_network_server():
+		if area.is_in_group("Ball") and area.get_parent().player_owner != int(name):
+			rpc("hit_by_damager", area.get_parent().damage)
+			
+			area.get_parent().rpc("destroy")
+
+sync func hit_by_damager(damage):
+	if get_tree().has_network_peer():
+		if get_tree().is_network_server():
+			rpc("destroy")
+
+
+sync func instance_bullet(id):
+	var player_bullet_instance = Global.instance_node_at_location(DAGGER, Players, self.global_position)
+	player_bullet_instance.name = "Bullet" + name + str(Network.networked_object_name_index)
+	player_bullet_instance.set_network_master(id)
+	player_bullet_instance.player_rotation = rotation
+	player_bullet_instance.player_owner = id
+	Network.networked_object_name_index += 1
